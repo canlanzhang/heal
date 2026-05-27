02ee919ecca3a0d7bd370d38ee97d242e1259d88
zhanghaiming <zhanghaiming@hotmail.com>
2026-05-27 22:49:43 +0800

    heal

--- crates/infrastructure/src/models.rs
+++ crates/infrastructure/src/models.rs
@@ -1,14 +1,27 @@
 
 use serde::{Serialize, Deserialize};
+use sqlx::FromRow;
+use chrono::{DateTime, Utc}; 
+
 
 #[derive(Debug, Deserialize)]
 pub struct CreateUserPayload {
     pub username: String,
 }
 
-
-#[derive(Debug, Serialize, Deserialize, Clone, sqlx::FromRow)]
+#[derive(Debug, FromRow, Serialize, Deserialize)]
 pub struct User {
     pub id: i32,
     pub username: String,
+    //pub created_at: DateTime<Utc>,
+    //pub updated_at: DateTime<Utc>, // 👈 加上这个
+}
+
+#[derive(Debug, FromRow, Serialize, Deserialize)]
+pub struct Todo {
+    pub id: i32,
+    pub title: String,
+    pub is_done: bool,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>, // 👈 加上这个
 }
