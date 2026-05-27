5e347b4daddbb3d5138d809049baf936dfaa4a50
zhanghaiming <zhanghaiming@hotmail.com>
2026-05-27 20:35:22 +0800

    heal

--- crates/app/src/routes.rs
+++ crates/app/src/routes.rs
@@ -2,16 +2,17 @@
 use axum::{routing::{get, post }, Router};
 use crate::state::AppState;
 use crate::handlers::{create_user_handler, handle_get_user};
-
+use tower_http::cors::CorsLayer;
 async fn health_check() -> &'static str {
     "OK"
 }
 
 pub fn get_router(state: AppState) -> Router {
     Router::new()
-        .route("/health", get(health_check))
-        .route("/users", post(create_user_handler))
-        .route("/user/{id}",get(handle_get_user))
+        .route("/api/health", get(health_check))
+        .route("/api/users", post(create_user_handler))
+        .route("/api/user/{id}",get(handle_get_user))
+        .layer(CorsLayer::permissive())
         .with_state(state) // Axum 0.8 标准的状态注入方式
 }
 
---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+import { ref, onMounted } from 'vue'
+
+const userInfo = ref<any>(null)
+  
+
+const message = ref('等待后端响应...')
+
+onMounted(async () => {
+  try {
+    // 假设你在 Axum 里有一个 GET /api/hello 的接口
+    const res = await fetch('/api/user/1')
+    const data = await res.json()
+    userInfo.value = data.data
+  } catch (e) {
+    console.error("连接失败", e)
+  }
+})
+
+</script>
+
+<template>asd
+  <div v-if="userInfo" class="card">
+      <h2>后端返回的用户：</h2>
+      <p>ID: {{ userInfo.id }}</p>
+      <p>用户名: {{ userInfo.username }}</p>
+    </div>
+  <HelloWorld />
+
+</template>
