36adef867b900cd48a9b9783d5cc2780e3791410
zhanghaiming <zhanghaiming@hotmail.com>
2026-06-23 12:14:30 +0800

    heal

--- frontend/src/bootstrap.ts
+++ frontend/src/bootstrap.ts
@@ -7,16 +7,16 @@ let inited = false
 export async function bootstrap() {
   if (inited) return
   inited = true
-
+console.log("1")
   const store = useUserStore()
-
+console.log("2")
   const token = localStorage.getItem('token')
   if (!token) return
-
+console.log("3")
   store.setToken(token)
-
+console.log("4")
   await store.fetchProfile()
-
+console.log("5")
   console.log('menus:', store.menus)
 
   const routes = buildDynamicRoutes(store.menus)
