c3623544ece4c7db5577b467369f9c3d6a807189
zhanghaiming <zhanghaiming@hotmail.com>
2026-06-28 12:35:09 +0800

    heal

--- crates/infrastructure/src/service.rs
+++ crates/infrastructure/src/service.rs
@@ -2,11 +2,11 @@
 
 pub mod auth_service;
 pub mod users_service;
-pub mod article_service;
-pub mod menu_service;
+pub mod articles_service;
+pub mod menus_service;
 
 // 可选统一导出
 pub use auth_service::*;
 pub use users_service::*;
-pub use article_service::*;
-pub use menu_service::*;
+pub use articles_service::*;
+pub use menus_service::*;
